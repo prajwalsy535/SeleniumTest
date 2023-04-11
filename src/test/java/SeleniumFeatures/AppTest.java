a5dcf79062b0334663e80d8c7ef0634a7d78155c
Prajwal S Y <prajwals@sti.com>
2023-04-11 14:33:43 +0530

    git2 development

--- src/test/java/SeleniumFeatures/AppTest.java
+++ src/test/java/SeleniumFeatures/AppTest.java
@@ -15,13 +15,20 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+    	
+    	//Git2 
+    	
         assertTrue( true );
         System.out.println("This is from git2");
         
         System.out.println("This is from git1");
         
-        System.out.println("This is from git1 developement");
-        System.out.println("This is from git1  developement");
-        System.out.println("This is from git1  developement");
+        System.out.println("This is from git1 development");
+        System.out.println("This is from git1  development");
+        System.out.println("This is from git1  development");
+        
+        System.out.println("This is from git2 development");
+        System.out.println("This is from git2  development");
+        System.out.println("This is from git2  development");
     }
 }
